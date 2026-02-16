dc3ed0aff85d8a7a455ec852568cf738317d94ed
Levin <levinmathewshibu@gmail.com>
2026-02-15 23:32:13 -0700

    spring

---
+++ cloud-asset-delivery/src/main/java/com/levin/cloud_asset_delivery/CloudAssetDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.levin.cloud_asset_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAssetDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudAssetDeliveryApplication.class, args);
+	}
+
+}
---
+++ cloud-asset-delivery/src/test/java/com/levin/cloud_asset_delivery/CloudAssetDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levin.cloud_asset_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudAssetDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
