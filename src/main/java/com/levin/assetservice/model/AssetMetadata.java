2d6c595314dfd119c1e0da9e28a98ce04e221512
Levin <levinmathewshibu@gmail.com>
2026-02-18 19:58:29 -0700

    aws config set up

--- src/main/java/com/levin/assetservice/model/AssetMetadata.java
+++ src/main/java/com/levin/assetservice/model/AssetMetadata.java
@@ -1,5 +1,19 @@
 package com.levin.assetservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AssetMetadata {
-    
+    private String key;
+    private String originalFileName;
+    private String contentType;
+    private long size;
+    private Instant uploadedAt;
+    private String cdnUrl;
 }
