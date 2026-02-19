81959831f01ad0dd44c108d775fc97db1c7552d4
Levin <levinmathewshibu@gmail.com>
2026-02-18 19:39:12 -0700

    pom fix

--- src/main/java/com/levin/assetservice/config/AwsConfig.java
+++ src/main/java/com/levin/assetservice/config/AwsConfig.java
@@ -1,3 +1,24 @@
-public class AwsConfig {
-    
-}
+package com.levin.assetservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+		public class AwsConfig {
+
+			@Value("${aws.region:us-east-1}")
+			private String awsRegion;
+
+			@Bean
+			public S3Client s3Client() {
+				return S3Client.builder()
+						.region(Region.of(awsRegion))
+						.credentialsProvider(DefaultCredentialsProvider.create())
+						.build();
+			}
+
+		}
