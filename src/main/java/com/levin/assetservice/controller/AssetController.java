e85771c07a974eacb782a969a0469da0e1b84a09
Levin <levinmathewshibu@gmail.com>
2026-03-06 17:21:14 -0700

    asset controller set up

--- src/main/java/com/levin/assetservice/controller/AssetController.java
+++ src/main/java/com/levin/assetservice/controller/AssetController.java
@@ -1,5 +1,31 @@
 package com.levin.assetservice.controller;
 
+import java.io.IOException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.levin.assetservice.model.AssetMetadata;
+
+@RestController
+@RequestMapping("/api")
 public class AssetController {
-    
+    private final s3AssetService s3AssetService;
+
+    public AssetController(s3AssetService s3AssetService) {
+        this.s3AssetService = s3AssetService;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<AssetMetadata> uploadAsset(@RequestParam("file") MultipartFile file) throws IOException {
+        AssetMetadata metadata = s3AssetService.uploadAsset(file);
+        if (file.isEmpty()) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(metadata);
+    }
+
+
 }
--- src/main/java/com/levin/assetservice/service/S3AssetService.java
+++ src/main/java/com/levin/assetservice/service/S3AssetService.java
@@ -1,5 +1,43 @@
 package com.levin.assetservice.service;
 
+import java.io.IOException;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.levin.assetservice.model.AssetMetadata;
+
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@Service
 public class S3AssetService {
-    
+    private final S3Client s3;
+    private final String bucket;
+    private final String cloudfrontDomain;
+
+    public S3AssetService(S3Client s3,@Value("${aws.s3.bucket}") String bucket,@Value("${aws.s3.bucket}") String cloudfrontDomain) {
+        this.s3 = s3;
+        this.bucket = bucket;
+        this.cloudfrontDomain = cloudfrontDomain;
+    }
+
+    public AssetMetadata upload(MultipartFile file)throws IOException {
+        String original=file.getOriginalFilename() == null ? "file" : file.getOriginalFilename();
+        String uuid = UUID.randomUUID().toString();
+        String key = uuid + "-" + original.replaceAll("\\s+", "_");
+
+        String cacheControl="public, max-age=31536000,immutable";
+
+        PutObjectRequest putReq=PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .cacheControl(cacheControl)
+                .build();
+
+        s3.putObject(putReq, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+        
+
+    }
 }
