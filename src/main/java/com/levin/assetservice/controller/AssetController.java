434cff4ab15e4ea1456b99349e6e045bdfffd1c3
Levin <levinmathewshibu@gmail.com>
2026-03-06 17:51:42 -0700

    improve AssetController code style and method naming

--- src/main/java/com/levin/assetservice/controller/AssetController.java
+++ src/main/java/com/levin/assetservice/controller/AssetController.java
@@ -2,25 +2,24 @@ package com.levin.assetservice.controller;
 
 import java.io.IOException;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-
 import com.levin.assetservice.model.AssetMetadata;
+import com.levin.assetservice.service.S3AssetService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api")
 public class AssetController {
-    private final s3AssetService s3AssetService;
+    private final S3AssetService s3AssetService;
 
-    public AssetController(s3AssetService s3AssetService) {
+    public AssetController(S3AssetService s3AssetService) {
         this.s3AssetService = s3AssetService;
     }
 
     @PostMapping("/upload")
     public ResponseEntity<AssetMetadata> uploadAsset(@RequestParam("file") MultipartFile file) throws IOException {
-        AssetMetadata metadata = s3AssetService.uploadAsset(file);
+        AssetMetadata metadata = s3AssetService.upload(file);
         if (file.isEmpty()) {
             return ResponseEntity.badRequest().build();
         }
